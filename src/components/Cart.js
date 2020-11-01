1aa74144756d82b3fec3ecb0c6ed4b64ecd18b9e
Marcin Stefanowicz <19smarcins98@gmail.com>
2020-11-01 09:28:53 +0100

    09:30 01.11.20

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import noImg from '../img/noImg.png'
+
+import '../css/Cart.css'
+
 const Cart = ({actualUser}) => {
-    console.log(actualUser);
+    const cartContent = actualUser.cart.map(book => (
+        <div className="cartItem">
+            <img src={book.volumeInfo.hasOwnProperty('imageLinks') ? book.volumeInfo.imageLinks.thumbnail : noImg} alt={book.volumeInfo.description}/>
+            <div className="description">
+                <h1>{book.volumeInfo.title}</h1>
+                <h2>{book.volumeInfo.authors}</h2>
+                <p>Cena: {book.saleInfo.saleability === "FOR_SALE" &&  book.saleInfo.listPrice.amount !== 0 ? book.saleInfo.listPrice.amount + " zł": "darmowa"}</p>
+            </div>
+        </div> 
+    ));
 
     return ( 
         <div className="cart">
-            
+            {cartContent}
         </div>
      );
 }
