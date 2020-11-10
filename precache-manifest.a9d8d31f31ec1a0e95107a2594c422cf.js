93fad4e477acdcb9643867dbdc808d5a48c24d56
Marcin Stefanowicz <19smarcins98@gmail.com>
2020-11-10 08:07:32 +0100

    Updates

---
+++ precache-manifest.a9d8d31f31ec1a0e95107a2594c422cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e58088e8895466319459e4c44cc3223",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "f9eabb84baa12215c217",
+    "url": "/bookstore/static/css/main.46309c67.chunk.css"
+  },
+  {
+    "revision": "13df9d904bad85cff185",
+    "url": "/bookstore/static/js/2.9b1e2b75.chunk.js"
+  },
+  {
+    "revision": "c937c4fa47a9982fa1ac3c3838ba40ff",
+    "url": "/bookstore/static/js/2.9b1e2b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9eabb84baa12215c217",
+    "url": "/bookstore/static/js/main.6a74ed62.chunk.js"
+  },
+  {
+    "revision": "01dfa390d09b3883de91",
+    "url": "/bookstore/static/js/runtime-main.2bf98abd.js"
+  }
+]);
